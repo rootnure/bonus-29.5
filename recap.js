df96407ddb27cdd3558d9343a9dee7b1993e4d1d
rootnure <nur.diu.2791@gmail.com>
2023-08-20 23:33:34 +0600

    recap of milestone 5

---
+++ recap.js
@@ -0,0 +1,27 @@
+/**
+ * 1. how to use js with html and css
+ * 
+ * 2. get element or elements from dom
+ *      2-1: getElementById
+ *      2-2: getElementsByClassName
+ *      2-3: querySelector
+ *      2-3: querySelectorAll
+ * 
+ * 3. dynamically add element to the dom: document.createElement
+ *      3-5: set value to the innerHTML
+ * 
+ * 4. get and set value from/to a dom element
+ *      4-1: use innerText (h1, p, h5, div, span, strong)
+ *      4-2: use value (input, textarea)
+ * 
+ * 5. add event listener 
+ *      5-1: click, focus, blur, onchange, keyup
+ *      5-2: addEventListener, onclick="myHandler()"
+ * 
+ * 6. user function to reduce duplicate code (DRY)
+ * 
+ * 7. ever bubble and event delegate
+ */
+
+document.querySelectorAll('.users-container .disabled h3');
+document.querySelectorAll('.my-users li');
