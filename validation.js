a1ffc3c1aeda4e96332c2789a17c694da68939e0
rootnure <nur.diu.2791@gmail.com>
2023-08-21 00:07:42 +0600

    typeof, Array.isArray(), isNaN(), WAT in JS

---
+++ validation.js
@@ -0,0 +1,18 @@
+const number = '23';
+// string, number, boolean, undefined, null
+if(typeof number === 'number') {
+    console.log('value is a number');
+}
+else {
+    console.log('value is a number')
+}
+
+const numbers = [65, 49, 68, 43, 16, 35, 41];
+const student = {name: 'Arafat', job: 'Teacher'};
+// console.log(Array.isArray(numbers));
+console.log(Array.isArray(student));
+console.log(typeof student);
+console.log(isNaN(number));
+console.log(isNaN(numbers))
+
+/* WAT in JS */
